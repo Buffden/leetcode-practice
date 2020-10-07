f86ac3ca1e4ecf29c3661798f7dc20ad0ff64bd9
Buffden <buffdenslays@gmail.com>
2020-10-07 09:28:51 +0000

    [LeetCode] 344. Reverse String

---
+++ two-pointers/0344-reverse-string/README.md
@@ -0,0 +1,13 @@
+# 344. Reverse String
+
+**Difficulty:** Easy
+**Link:** https://leetcode.com/problems/reverse-string/
+**Topics:** Two Pointers, String
+
+## Stats
+- Runtime: 28 ms (beats 2.08%)
+- Memory: 25.6 MB (beats 100.00%)
+
+## Approach
+
+## Complexity
---
+++ two-pointers/0344-reverse-string/reverse-string-solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverse( vector<char> &s,int left, int right)
+    {
+        if(left>=right)
+            return;
+        char temp=s[left];
+        s[left]=s[right];
+        s[right]=temp;
+        reverse(s,++left,--right);
+    }
+    void reverseString(vector<char>& s) {
+        reverse(s,0,s.size()-1);
+        return;
+    }
+};
