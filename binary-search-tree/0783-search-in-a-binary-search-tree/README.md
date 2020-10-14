16e155d327f46ffc62a7f751674a99949f7f4c50
Buffden <buffdenslays@gmail.com>
2020-10-14 06:28:13 +0000

    [LeetCode] 783. Search in a Binary Search Tree

---
+++ binary-search-tree/0783-search-in-a-binary-search-tree/README.md
@@ -0,0 +1,13 @@
+# 783. Search in a Binary Search Tree
+
+**Difficulty:** Easy
+**Link:** https://leetcode.com/problems/search-in-a-binary-search-tree/
+**Topics:** Tree, Binary Search Tree, Binary Tree
+
+## Stats
+- Runtime: 52 ms (beats 0.03%)
+- Memory: 35.4 MB (beats 95.07%)
+
+## Approach
+
+## Complexity
---
+++ binary-search-tree/0783-search-in-a-binary-search-tree/search-in-a-binary-search-tree-solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        TreeNode* ans= present(root,val);
+        return ans;
+    }
+    TreeNode* present(TreeNode* root, int val)
+    {
+        if(!root)
+            return root;
+        if(root->val==val)
+            return root;
+        return ((root->val)<val?present(root->right,val):present(root->left,val));
+    }
+};
