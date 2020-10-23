843be3d3e77e497ec653b396bc2c57b380043ecf
Buffden <buffdenslays@gmail.com>
2020-10-23 08:20:38 +0000

    [LeetCode] 21. Merge Two Sorted Lists

---
+++ recursion/0021-merge-two-sorted-lists/README.md
@@ -0,0 +1,13 @@
+# 21. Merge Two Sorted Lists
+
+**Difficulty:** Easy
+**Link:** https://leetcode.com/problems/merge-two-sorted-lists/
+**Topics:** Linked List, Recursion
+
+## Stats
+- Runtime: 4 ms (beats 1.54%)
+- Memory: 15.3 MB (beats 100.00%)
+
+## Approach
+
+## Complexity
---
+++ recursion/0021-merge-two-sorted-lists/merge-two-sorted-lists-solution.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* S;
+        ListNode* newhead=NULL;
+        if(l1==NULL)
+            return l2;
+        if(l2==NULL)
+            return l1;
+        if(l1&&l2)
+        {
+            if(l1->val<=l2->val)
+            {
+                S=l1;
+                l1=l1->next;
+            }
+            else{
+                S=l2;
+                l2=l2->next;
+            }
+            newhead=S;
+        }
+        while(l1&&l2)
+        {
+            if(l1->val<=l2->val)
+            {
+                S->next=l1;
+                S=l1;
+                l1=S->next;
+            }
+            else{
+                S->next=l2;
+                S=l2;
+                l2=S->next;
+            }
+        }
+        if(l1==NULL) S->next=l2;
+        if(l2==NULL) S->next=l1;
+        return newhead;
+    }
+};
