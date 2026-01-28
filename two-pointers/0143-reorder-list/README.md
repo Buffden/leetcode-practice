792c6b1877cfbec102edf31e2e612dabc18202f0
Buffden <buffdenslays@gmail.com>
2026-01-28 11:44:56 +0000

    [LeetCode] 143. Reorder List

---
+++ two-pointers/0143-reorder-list/README.md
@@ -0,0 +1,13 @@
+# 143. Reorder List
+
+**Difficulty:** Medium
+**Link:** https://leetcode.com/problems/reorder-list/
+**Topics:** Linked List, Two Pointers, Stack, Recursion
+
+## Stats
+- Runtime: 2 ms (beats 87.49%)
+- Memory: 49.2 MB (beats 53.47%)
+
+## Approach
+
+## Complexity
---
+++ two-pointers/0143-reorder-list/reorder-list-solution.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode first = head;
+        ListNode second = reverseListNode(slow.next);
+        slow.next = null; // important to break the link
+
+        // stopping at second as null because the first half may contain an extra element 
+        // incase of odd number of elements in the ListNode
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+
+    }
+
+    public static ListNode reverseListNode (ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+
+        while (curr != null) {
+            next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
