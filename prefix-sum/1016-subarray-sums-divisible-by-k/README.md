e97f890d78bd5d2264001b49ba8e833f86fe1e84
Buffden <buffdenslays@gmail.com>
2026-04-29 01:48:02 +0000

    [LeetCode] 1016. Subarray Sums Divisible by K

---
+++ prefix-sum/1016-subarray-sums-divisible-by-k/README.md
@@ -0,0 +1,13 @@
+# 1016. Subarray Sums Divisible by K
+
+**Difficulty:** Medium
+**Link:** https://leetcode.com/problems/subarray-sums-divisible-by-k/
+**Topics:** Array, Hash Table, Prefix Sum
+
+## Stats
+- Runtime: 24 ms (beats 66.33%)
+- Memory: 51.5 MB (beats 66.61%)
+
+## Approach
+
+## Complexity
---
+++ prefix-sum/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k-solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int prefix = 0;
+        int rem = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix += nums[i];
+
+            rem = prefix % k;
+            if (rem < 0) {
+                rem += k;
+            }
+
+            if (map.containsKey(rem)) {
+                count += map.get(rem);
+            }
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        }
+        return count;
+    }
+}
