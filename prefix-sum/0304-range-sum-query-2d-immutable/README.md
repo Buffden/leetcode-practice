c6806f5af28c41167f38c4eecb7c626f188cbef4
Buffden <buffdenslays@gmail.com>
2026-04-30 04:36:30 +0000

    [LeetCode] 304. Range Sum Query 2D - Immutable

---
+++ prefix-sum/0304-range-sum-query-2d-immutable/README.md
@@ -0,0 +1,13 @@
+# 304. Range Sum Query 2D - Immutable
+
+**Difficulty:** Medium
+**Link:** https://leetcode.com/problems/range-sum-query-2d-immutable/
+**Topics:** Array, Design, Matrix, Prefix Sum
+
+## Stats
+- Runtime: 99 ms (beats 93.01%)
+- Memory: 143.9 MB (beats 5.22%)
+
+## Approach
+
+## Complexity
---
+++ prefix-sum/0304-range-sum-query-2d-immutable/range-sum-query-2d-immutable-solution.java
@@ -0,0 +1,26 @@
+class NumMatrix {
+    int[][] prefix; // global
+
+    public NumMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        prefix = new int[rows + 1][cols + 1];
+
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= cols; col++) {
+                prefix[row][col] = matrix[row - 1][col - 1] + prefix[row - 1][col] + prefix[row][col - 1] - prefix[row - 1][col - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix[row2 + 1][col2 + 1] - prefix[row1][col2 + 1] - prefix[row2 + 1][col1] + prefix[row1][col1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
