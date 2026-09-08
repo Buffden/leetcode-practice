c4e3c2c0d7a3006d955fae2817732811cd640a11
Buffden <buffdenslays@gmail.com>
2026-09-08 21:21:40 +0000

    [LeetCode] 1. Two Sum

--- hash-table/0001-two-sum/two-sum-solution.java
+++ hash-table/0001-two-sum/two-sum-solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> myFreq = new HashMap<Integer, Integer>(); // number -> index
+        HashMap<Integer, Integer> myFreq = new HashMap<Integer, Integer>(); // map number to its index
         HashSet<Integer> mySet = new HashSet<Integer>(); // for not choosing the same number that we added in map twice
         int[] ansArray = new int[2];
 
@@ -9,7 +9,7 @@ class Solution {
                 mySet.add(nums[index]);
                 myFreq.put(nums[index], index);
             }
-            int difference = target - nums[index]; // we will find this difference in the map that holds its index too
+            int difference = target - nums[index]; // we will find this difference in the map that holds the number's index too
             if (mySet.contains(difference) && myFreq.get(difference) != index) {
                 ansArray[0] = index;
                 ansArray[1] = myFreq.get(difference);
